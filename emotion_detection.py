13540a889aad8513846b818bb39d00d3a1daf1af
atamazian <9746588+atamazian@users.noreply.github.com>
2026-02-23 03:57:41 -0500

    modified emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 def emotion_detector(text_to_analyze):
@@ -5,4 +6,20 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=headers)
-    return response.text
+    formatted_response = json.loads(response.text)
+    emotion_data = formatted_response["emotionPredictions"][0]["emotion"]
+    anger_score = emotion_data["anger"]
+    disgust_score = emotion_data["disgust"]
+    fear_score = emotion_data["fear"]
+    joy_score = emotion_data["joy"]
+    sadness_score = emotion_data["sadness"]
+    dominant_emotion = max(emotion_data, key=emotion_data.get)
+    result = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
+    return result
